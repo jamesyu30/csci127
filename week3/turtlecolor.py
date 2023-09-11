4da43167885b4d4fe1a6f693363e8876359881a6
James Yu <jyu30@stuy.edu>
2023-09-11 15:02:54 -0400

    hw 11-12

---
+++ week3/turtlecolor.py
@@ -0,0 +1,12 @@
+#Name:  James Yu
+#Email:  james.yu66@myhunter.cuny.edu
+#Date: September 11, 2023
+
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+
+res = input("Enter a color: ")
+
+t.color(res)
