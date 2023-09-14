d55eb3dadcec1ea399043f852d6d00b0c9383ec6
James Yu <jyu30@stuy.edu>
2023-09-13 21:42:15 -0400

    hw 11-13

---
+++ week3/pentagon.py
@@ -0,0 +1,13 @@
+#Name:  James Yu
+#Email:  james.yu66@myhunter.cuny.edu
+#Date: September 13, 2023
+
+import turtle
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("cornflowerblue")
+for i in range(5):
+    t.forward(100)
+    t.left(360/5)
+    t.stamp()
