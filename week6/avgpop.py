f8372b51ff066171791fcab2e5e468244b46a36c
James Yu <jyu30@stuy.edu>
2023-10-05 11:59:43 -0400

    hw 25-30

--- week6/avgpop.py
+++ week6/avgpop.py
@@ -5,8 +5,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-pop = pd.read_csv('nycHistpop.csv', skiprows=5)
+pop = pd.read_csv('nycHistPop.csv', skiprows=5)
 borough = input("Enter a borough: ")
 
 print(pop[borough].mean())
-print(int(pop[borough].max()))
+print(pop[borough].max())
