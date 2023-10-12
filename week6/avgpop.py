2cf01d100f809eeabcebee7e0bd92f33096cae3c
James Yu <jyu30@stuy.edu>
2023-10-12 13:48:12 -0400

    hw 28

--- week6/avgpop.py
+++ week6/avgpop.py
@@ -2,7 +2,7 @@
 #Email:  james.yu66@myhunter.cuny.edu
 #Date: October 5, 2023
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pandas as pd
 
 pop = pd.read_csv('nycHistPop.csv', skiprows=5)
