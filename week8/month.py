44386b5bf4a6c9c7b4e3196947ca401e7e23a5f3
James Yu <jyu30@stuy.edu>
2023-10-25 23:19:06 -0400

    hw 38

---
+++ week8/month.py
@@ -0,0 +1,29 @@
+#Name:  James Yu
+#Email:  james.yu66@myhunter.cuny.edu
+#Date: October 25, 2023
+# A program that uses functions to print out months.
+
+def monthString(monthNum):
+     """
+     Takes as input a number, monthNum, and
+     returns the corresponding month name as a string.
+     Example: monthString(1) returns "January".
+     Assumes that input is an integer ranging from 1 to 12
+     """
+     
+     monthString = ""
+
+     monthString = ["January", "February", "March", "April", "May", "June", "July",\
+                     "August", "September", "October", "November", "December"][monthNum-1]
+
+     return(monthString)
+
+def main():
+     n = int(input('Enter the number of the month: '))
+     mString = monthString(n)
+     print('The month is', mString)
+
+#Allow script to be run directly:
+if __name__ == "__main__":
+     main()
+                   
